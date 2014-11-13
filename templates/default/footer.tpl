ac7cd74ad5ebd51c24c9f1403ac1a9587192a449
W.F. Yang <jesonyang001@yahoo.com.hk>
2014-11-13 22:40:16 +0800

    test

--- templates/default/footer.tpl
+++
@@ -1,6 +0,0 @@
-</div>
-<div class="col footer align_left">
-	<a href="https://github.com/jesonyang001" target="_blank">{$LANG.powered_by_jy}</a>
-</div>
-</body>
-</html>
--- tmp/cache/%%01^01F^01F8B381%%view.tpl.php
+++
@@ -1,8 +0,0 @@
-<?php /* Smarty version 2.6.18, created on 2013-12-12 00:14:25
-         compiled from ../templates/default/documentation/view.tpl */ ?>
-<div id="left">
-<p>
-    <?php echo $this->_tpl_vars['page']; ?>
-
-</p>
-</div>
